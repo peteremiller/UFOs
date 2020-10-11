3449dc40ae6457cf041974c71db04fc8cdd86e21
Peter Miller <peteremiller@outlook.com>
2020-10-11 00:02:47 -0500

    Updating files for challenge 11.

--- static/js/app.js
+++ static/js/app.js
@@ -34,19 +34,20 @@ function updateFilters() {
     // 4b. Save the value that was changed as a variable.
   let elementValue = changedElement.property("value");
   console.log(elementValue);
-    // 4c. Save the id of the filter that was changed as a variable.
+
+  // 4c. Save the id of the filter that was changed as a variable.
   let filterId = changedElement.attr("id");
-  console.log(filterId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-  if (ElementValue) {
+  if (elementValue) {
     filters[filterId] = elementValue;
   }
   else {
     delete filters[filterId];
   }
-    // 6. Call function to apply all filters and rebuild the table
+  
+  // 6. Call function to apply all filters and rebuild the table
     filterTable();
 
   }
@@ -59,13 +60,12 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    Object.entries(filters).forEach(([key,value]) => {
-      filteredData = filterData.filter(row => row[key] === value);
-      
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
     });
   
     // 10. Finally, rebuild the table using the filtered data
-    buildTable(filtereData);
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
